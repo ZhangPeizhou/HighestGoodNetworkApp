da08375a205161eac49760cb878a61e0558d111d
lacnoskillz <kaikane3457@hotmail.com>
2023-12-29 15:00:13 -0600

    made page better match figma. working on calendar functionality eslint issues fixed

--- src/components/BMDashboard/AddConsumable/AddConsumable.jsx
+++ src/components/BMDashboard/AddConsumable/AddConsumable.jsx
@@ -1,81 +1,189 @@
-import { Col, Container, Form, FormGroup, Input, Label, Button, CardBody, Card } from 'reactstrap';
 import './AddConsumable.css';
-import { toast } from 'react-toastify';
+import { useState } from 'react';
+import { Form, FormControl, Button } from 'react-bootstrap';
+import './AddConsumable.css';
+import DayPickerInput from 'react-day-picker/DayPickerInput';
+import dateFnsFormat from 'date-fns/format';
+import { FaCalendarAlt } from 'react-icons/fa';
+import Noimg from '../Lesson/images/Noimg3.jpg';
 
+const style = {
+  backgroundImage: `url(${Noimg})`,
+};
 function AddConsumable() {
-  const changeHandler = () => {
-    // console.log('change handler');
+  const FORMAT = 'MM/dd/yy';
+  const [selectedFile, setSelectedFile] = useState(null);
+  const [selectedDate, setSelectedDate] = useState(null);
+  const formatDate = (date, format, locale) => dateFnsFormat(date, format, { locale });
+  // TODO fix handleDrop
+  // const handleDrop = (e) => {
+  //   e.preventDefault();
+  //   e.stopPropagation(); // Prevent the default behavior
+  //   console.log("handledrop")
+  //   const file = e.target.files[0]; // Get the selected file
+  //   setSelectedFile(file);
+  // };
+
+  // const handleDragOver = e => {
+  //   e.preventDefault();
+  //   // console.log("hi")
+  // };
+  const handleFileSelection = e => {
+    const file = e.target.files[0]; // Get the selected file
+    setSelectedFile(file); // Update the state with the selected file
   };
 
-  const submitHandler = () => {
-    toast.success('submit handler');
+  const handleClick = () => {
+    // console.log(selectedFile,"hi click")
+    const fileInput = document.getElementById('fileInput');
+    if (fileInput) {
+      fileInput.click();
+    }
+  };
+  const handleKeyPress = () => {
+    // console.log("hi Keypress")
   };
 
+  // const handleMouseDown = () => {
+  //   console.log("hi mousedown")
+  // };
+
+  // const handleTouchStart = () => {
+  //   console.log("hi touchstart")
+  // };
   return (
-    <div>
-      <Container fluid className="materialContainer">
-        <div className="materialPage">
-          <div className="material">
-            <div className="materialTitle">ADD Consumable FORM</div>
-            <Card>
-              <CardBody>
-                <Form id="AddMaterialForm">
-                  <FormGroup row className="align-items-center justify-content-start">
-                    <Label for="" lg={2} sm={4} className="materialFormLabel">
-                      Item Type
-                    </Label>
-                    <Col lg={4} sm={8} className="materialFormValue">
-                      <Input
-                        id=""
-                        name=""
-                        type="text"
-                        placeholder="Consumable Name"
-                        value="Consumable"
-                        disabled
-                      />
-                    </Col>
-                  </FormGroup>
-                  <FormGroup row className="align-items-center justify-content-start">
-                    <Label for="name" lg={2} sm={4} className="materialFormLabel">
-                      Consumable Name
-                    </Label>
-                    <Col lg={4} sm={8} className="materialFormValue">
-                      <Input
-                        id="name"
-                        name="name"
-                        type="text"
-                        onChange={e => changeHandler(e)}
-                        placeholder="Consumable Name"
-                      />
-                    </Col>
-                  </FormGroup>
-                  <FormGroup row className="align-items-center justify-content-start">
-                    <Label for="description" lg={2} sm={4} className="materialFormLabel">
-                      Consumable Description
-                    </Label>
-                    <Col lg={4} sm={8} className="materialFormValue">
-                      <Input
-                        id="description"
-                        name="description"
-                        type="text"
-                        placeholder="Consumable description"
-                        onChange={e => changeHandler(e)}
-                      />
-                    </Col>
-                  </FormGroup>
-
-                  <FormGroup row className="d-flex justify-content-right">
-                    <Button onClick={() => submitHandler()} className="materialButtonBg">
-                      Add Consumable
-                    </Button>
-                  </FormGroup>
-                </Form>
-              </CardBody>
-            </Card>
-            <hr />
+    <div className="ConsumableMasterContainer">
+      <div className="ConsumableFormContainer">
+        <Form>
+          <Form.Label className="ConsumableLabel">Add Consumable</Form.Label>
+          <div className="ConsumableFormSelectContainer">
+            <div className="ConsumableSingleFormSelect">
+              <Form.Group controlId="Form.ControlSelect1">
+                <Form.Label>Project</Form.Label>
+                <FormControl as="select" aria-label="Default select example">
+                  <option>All</option>
+                  <option value="1">One</option>
+                  <option value="2">Two</option>
+                  <option value="3">Three</option>
+                </FormControl>
+              </Form.Group>
+            </div>
+            <div className="ConsumableSingleFormSelect">
+              <Form.Group controlId="Form.ControlSelect2">
+                {/* By default the lesson can be read by anyone. The author can change the permission to only public to certain roles. */}
+                <Form.Label>Consumable Name</Form.Label>
+                <FormControl as="select" aria-label="Default select example">
+                  <option>All</option>
+                  <option value="1">One</option>
+                  <option value="2">Two</option>
+                  <option value="3">Three</option>
+                </FormControl>
+              </Form.Group>
+              <Form.Group controlId="exampleForm.ControlInput1">
+                {/* Can have multiple tags. Needs to add tag for belonging project name automatically. */}
+                <Form.Label>Invoice Number or ID</Form.Label>
+                <Form.Control type="text" placeholder="Invoice No or ID for consumable" />
+              </Form.Group>
+            </div>
+            <div className="ConsumablePriceDiv">
+              <Form.Group controlId="exampleForm.ControlInput1">
+                <Form.Label>Unit Price (exct. taxes & shipping)</Form.Label>
+                <Form.Control type="text" placeholder="Invoice No or ID for consumable" />
+              </Form.Group>
+              <Form.Group>
+                {/* By default the lesson can be read by anyone. The author can change the permission to only public to certain roles. */}
+                <Form.Label>Currency</Form.Label>
+                <FormControl as="select" aria-label="Default select example">
+                  <option>USD</option>
+                  <option value="1">One</option>
+                  <option value="2">Two</option>
+                  <option value="3">Three</option>
+                </FormControl>
+              </Form.Group>
+              <Form.Group controlId="exampleForm.ControlInput1">
+                {/* Can have multiple tags. Needs to add tag for belonging project name automatically. */}
+                <Form.Label>Invoice Number or ID</Form.Label>
+                <Form.Control type="text" placeholder="Invoice No or ID for consumable" />
+              </Form.Group>
+            </div>
+            <div className="ConsumableCurrencySelect">
+              <Form.Group>
+                {/* By default the lesson can be read by anyone. The author can change the permission to only public to certain roles. */}
+                <Form.Label>Qty Unit of Measurement</Form.Label>
+                <FormControl as="select" aria-label="Default select example">
+                  <option>All</option>
+                  <option value="1">One</option>
+                  <option value="2">Two</option>
+                  <option value="3">Three</option>
+                </FormControl>
+              </Form.Group>
+              <Form.Group>
+                <Form.Label>Purchased Date</Form.Label>
+                <div className="date-picker-wrapper">
+                  <DayPickerInput
+                    selected={selectedDate}
+                    // eslint-disable-next-line
+                    onChange={(date) => setSelectedDate(date)}
+                    placeholder={`${dateFnsFormat(new Date(), FORMAT)}`}
+                    dateFormat="MM/dd/yy"
+                    formatDate={formatDate}
+                    value={selectedDate}
+                  />
+                  <FaCalendarAlt
+                    className="calendar-icon"
+                    onClick={() => document.getElementById('datePicker').click()}
+                  />
+                  <input
+                    id="datePicker"
+                    type="hidden"
+                    onFocus={() => document.getElementById('datePicker').click()}
+                  />
+                </div>
+              </Form.Group>
+            </div>
           </div>
-        </div>
-      </Container>
+          <div className="ConsumableDragAndDropFormGroup">
+            <Form.Group controlId="exampleForm.ControlFile1">
+              <Form.Label>Upload Appendix</Form.Label>
+              <input
+                type="file"
+                id="fileInput"
+                style={{ display: 'none' }}
+                onChange={handleFileSelection}
+              />
+              <div
+                role="button"
+                tabIndex={0}
+                onClick={handleClick}
+                onKeyPress={handleKeyPress}
+                // onMouseDown={handleMouseDown}
+                // onTouchStart={handleTouchStart}
+                // onDrop={handleDrop}
+                // onDragOver={handleDragOver}
+                className="ConsumabledragAndDropStyle"
+              >
+                {selectedFile ? (
+                  <p>Selected File: {selectedFile.name}</p>
+                ) : (
+                  <div className="ConsumableTextAndImageDiv">
+                    <div className="ConsumableImageDiv" style={style} />
+
+                    <p className="ConsumableDragandDropText">Drag and drop a file here</p>
+                  </div>
+                )}
+              </div>
+            </Form.Group>
+          </div>
+          <div className="ConsumableButtonDiv">
+            <Button className="ConsumableFormButtonCancel" type="cancel">
+              Cancel
+            </Button>
+            <Button className="ConsumableFormButtonSubmit" type="submit">
+              Submit
+            </Button>
+          </div>
+        </Form>
+      </div>
     </div>
   );
 }
